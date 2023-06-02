3dfcaa33de053f591c9994081eeab0e6bd934509
Gabriel Reimundo <109987851+gabrielreimundo@users.noreply.github.com>
2023-06-02 06:59:33 -0300

    at

--- index.js
+++ index.js
@@ -109,7 +109,9 @@ fontSizes.forEach(size=>{
             root.style.setProperty('--sticky-top-left', '-12rem')
             root.style.setProperty('--sticky-top-right', '-35rem')
         }
+
+        // change font size of the root html element
+        document.querySelector('html').style.fontSize=fontSize;
     })
-    // change font size of the root html element
-    document.querySelector('html').style.fontSize=fontSize;
+
 })
