f8947ee924f7c92e8e11f885f6e36362944c1e30
Gabriel Reimundo <109987851+gabrielreimundo@users.noreply.github.com>
2023-06-02 06:40:53 -0300

    at

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const messages=document.querySelector('.messages')
 const message=messages.querySelectorAll('.message')
 const messageSearch=document.querySelector('#message-search')
 
+// THEME    
+const theme=document.querySelector('#theme')
+const themeModal = document.querySelector('.customize-theme');
+
 
 //=============== SIDEBAR ============
 
@@ -56,3 +60,22 @@ messagesNotification.addEventListener('click',()=>{
         messages.style.boxShadow='none';
     },2000)
 })
+
+// THEME/DISPLAY CUSTOMIZATION
+
+// opens modal
+const openThemeModal=()=>{
+    themeModal.style.display='grid'
+}
+
+// closes modal
+const closeThemeModal=(e)=>{
+    if(e.target.classList.contains('customize-theme')){
+        themeModal.style.display='none';
+    }
+}
+
+// close modal
+themeModal.addEventListener('click', closeThemeModal)
+
+theme.addEventListener('click',openThemeModal)
